f2fbee6a899f285b668bc52c3fa1fc80b9491542
jamiltron <justinanthonyhamilton@gmail.com>
2011-07-14 00:34:20 -0500

    adding problems

---
+++ solutions.clj
@@ -0,0 +1,21 @@
+;; This only contains problems that require an actual function
+
+;; Double Down: Write a function which doubles a number.
+(defn double-down [x]
+  (* x 2))
+
+;; Hello World: Write a function which returns a personalized greeting.
+(defn hello-world [name]
+  (str "Hello, " name "!"))
+
+;; Last Element: Write a function which returns the last element in a sequence.
+(defn last-element [x]
+  (nth x (dec (count x))))
+
+;; Penultimate Element: Write a function which returns the second to last element from a sequence.
+(defn pen-elem [x]
+  (nth x (- (count x) 2)))
+
+;; Nth element: Write a function which returns the Nth element from a sequence.
+(defn nth-elem [s x]
+  (last (take (inc x) s)))
